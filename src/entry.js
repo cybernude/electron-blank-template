05e8a758dd83ca6a64c2226bfd1fcf281b3e1e3c
cybernude <ninetea@gmail.com>
2017-08-01 22:23:13 +0700

    add all new code electron blank template

---
+++ src/entry.js
@@ -0,0 +1,75 @@
+const {app, BrowserWindow, ipcMain, dialog} = require('electron')
+// require('electron-dl')();
+const path = require('path')
+const url = require('url')
+
+// const {download} = require('electron-dl');
+let win
+
+ipcMain.on('close-app', (event, arg) => {
+  event.returnValue = null
+  app.quit();
+});
+
+ipcMain.on('open-devtools', (event, arg) => {
+  event.returnValue = null
+  win.webContents.openDevTools();
+});
+
+ipcMain.on('open-token-path', (event, arg) => {
+  
+  event.returnValue = dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [ {name: 'Text file', extensions: ['txt']} ]
+  });
+});
+
+ipcMain.on('download-file', (event, args) => {
+  const options = {
+    saveAs: true,
+    openFolderWhenDone: true
+  }
+	download(BrowserWindow.getFocusedWindow(), args.url, options)
+    .then(dl => {
+      console.log(dl.getSavePath());
+      let result = { ok: true, path: dl.getSavePath() };
+      event.sender.send('downloaded', result);
+    })
+		.catch((err) => event.sender.send('downloaded', {ok: false, message: err}));
+});
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Smart Target: ระบบสนับสนุนบริการสุขภาพ และ แลกเปลี่ยนข้อมูล',
+  })
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  
+  win.maximize();
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
---
+++ src/entry.live.js
@@ -0,0 +1,78 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+// require('electron-dl')();
+
+const path = require('path')
+const url = require('url')
+
+// const {download} = require('electron-dl');
+let win
+
+ipcMain.on('close-app', (event, arg) => {
+  event.returnValue = null
+  app.quit();
+});
+
+ipcMain.on('open-devtools', (event, arg) => {
+  event.returnValue = null
+  win.webContents.openDevTools();
+});
+
+ipcMain.on('open-token-path', (event, arg) => {
+  
+  event.returnValue = dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [ {name: 'Text file', extensions: ['txt']} ]
+  });
+});
+
+ipcMain.on('download-file', (event, args) => {
+  const options = {
+    saveAs: true,
+    openFolderWhenDone: true
+  }
+	download(BrowserWindow.getFocusedWindow(), args.url, options)
+    .then(dl => {
+      console.log(dl.getSavePath());
+      let result = { ok: true, path: dl.getSavePath() };
+      event.sender.send('downloaded', result);
+    })
+		.catch((err) => event.sender.send('downloaded', {ok: false, message: err}));
+});
+
+function createWindow () {
+  setTimeout(() => {
+    // Create the browser window.
+    win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      title: 'Smart Target: ระบบสนับสนุนบริการสุขภาพ และ แลกเปลี่ยนข้อมูล'
+    });
+
+    win.loadURL(url.format({
+      pathname: 'localhost:4200',
+      protocol: 'http:',
+      slashes: true
+    }));
+
+    win.maximize();    
+    // win.webContents.openDevTools()
+
+    win.on('closed', () => {
+      win = null
+    })
+  }, 12000)
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
